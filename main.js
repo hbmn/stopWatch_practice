cfb88fe37d105d159206be1bd54cd8dd34e63539
hirabayashimana <hbmn5415@gmail.com>
2022-11-12 14:00:42 +0900

    デバッグ

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ const display = document.querySelector(".display");
 let stop = null;
 
 startBtn.addEventListener("click", () => {
-  let seconds = 0;
-  stop = setInterval(() => {
-    seconds++;
-    display.textContent = seconds;
-  }, 1000);
+  if (stop === null) {
+    let seconds = 0;
+    stop = setInterval(() => {
+      seconds++;
+      display.textContent = seconds;
+    }, 1000);
+  }
 });
 
 stopBtn.addEventListener("click", () => {
-  clearInterval(stop);
+  if (stop !== null) {
+    clearInterval(stop);
+    stop = null;
+  }
 });
